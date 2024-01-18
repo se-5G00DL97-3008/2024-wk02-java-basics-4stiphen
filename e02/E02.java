ee2bf0d7e31a2d2c413638881a5246604a065aff
Stephen Duru <4stiphen@gmail.com>
2024-01-18 09:29:31 +0000

    commit 1-5

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 class E02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Give another number:");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        int product = num1 * num2;
+        int minus = num1 - num2;
+        int divide = num1 / num2;
+
+        System.out.print(num1);
+        System.out.print(" + ");
+        System.out.print(num2);
+        System.out.print(" = ");
+        System.out.println(sum);
+        
+        System.out.print(num1);
+        System.out.print(" * ");
+        System.out.print(num2);
+        System.out.print(" = ");
+        System.out.println(product);
+
+        System.out.print(num1);
+        System.out.print(" - ");
+        System.out.print(num2);
+        System.out.print(" = ");
+        System.out.println(minus);
+
+        System.out.print(num1);
+        System.out.print(" / ");
+        System.out.print(num2);
+        System.out.print(" = ");
+        System.out.println(divide);
+        
+        
+        
 
     }
 }
